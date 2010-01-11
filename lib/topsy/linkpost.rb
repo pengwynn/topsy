cedba3583117609d31e5371cd50120c173e41f66
Wynn Netherland <wynn.netherland@gmail.com>
2010-01-11 15:47:01 -0600

    Updated comments for linkpost

--- lib/topsy/linkpost.rb
+++ lib/topsy/linkpost.rb
@@ -1,15 +1,14 @@
-# This is the linkpost class for the topsy library. 
-# A Linkpost has the following attributes:
-#            "permalink_url" : "http://twitter.com/barackobama/status/3569838653",
-#            "target" : target instance
-#            "date" : "1251337427",
-#            "content" : "Highlights from the tribute to Sen. Kennedy's life and 
-#                accomplishments from last year's Democratic National Convention: http://bit.ly/TJcyr",
-#            "date_alpha" : "3 hours ago"
-# According to the official Topsy doc: http://code.google.com/p/otterapi/wiki/Resources?tm=6#/linkposts
-#
+
 module Topsy
-  
+  # This is the linkpost class for the topsy library. 
+  # A Linkpost has the following attributes:
+  #   "permalink_url" : "http://twitter.com/barackobama/status/3569838653",
+  #   "target" : target instance
+  #   "date" : "1251337427",
+  #   "content" : "Highlights from the tribute to Sen. Kennedy's life and 
+  #                accomplishments from last year's Democratic National Convention: http://bit.ly/TJcyr",
+  #   "date_alpha" : "3 hours ago"
+  # According to the official Topsy doc: http://code.google.com/p/otterapi/wiki/Resources?tm=6#/linkposts
   class Linkpost < Hashie::Dash
     property :permalink_url
     property :target
